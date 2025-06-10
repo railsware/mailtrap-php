2936a5e4bb1f26cd9f1d5c25b3e2825c51505dbb
gaalferov <admin@gaalferov.com>
2025-06-10 18:19:24 +0200

    add new instance provider into tests

--- tests/MailtrapGeneralClientTest.php
+++ tests/MailtrapGeneralClientTest.php
@@ -28,7 +28,7 @@ class MailtrapGeneralClientTest extends MailtrapClientTestCase
     {
         foreach (MailtrapGeneralClient::API_MAPPING as $key => $item) {
             yield match ($key) {
-                'permissions', 'users', 'contacts' => [new $item($this->getConfigMock(), self::FAKE_ACCOUNT_ID)],
+                'permissions', 'users', 'contacts', 'emailTemplates' => [new $item($this->getConfigMock(), self::FAKE_ACCOUNT_ID)],
                 default => [new $item($this->getConfigMock())],
             };
         }
