e4cab763955dd4d5bc3ab706a8ce5c5027ae15f5
gaalferov <admin@gaalferov.com>
2025-07-12 14:29:47 +0200

    update readme in example

--- examples/general/contacts.php
+++ examples/general/contacts.php
@@ -275,7 +275,10 @@ try {
 
 
 /**
- * Import contacts in bulk.
+ * Import contacts in bulk with support for custom fields and list management.
+ * Existing contacts with matching email addresses will be updated automatically.
+ * You can import up to 50,000 contacts per request.
+ * The import process runs asynchronously - use the returned import ID to check the status and results.
  *
  * POST https://mailtrap.io/api/accounts/{account_id}/contacts/imports
  */
